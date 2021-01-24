22cb5f8e4de4747a336119d79c92bedea4e3d21d
blDmWAKq <iso2001@gmail.com>
2021-01-24 16:34:25 +0200

    JPA structure completed. Linted. No cascades.

--- src/main/java/ua/od/ones/entity/UserIssue.java
+++ src/main/java/ua/od/ones/entity/UserIssue.java
@@ -8,10 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 @Data
