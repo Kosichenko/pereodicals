983f96d2d9bdce214051cf9f10ca931080128de2
blDmWAKq <iso2001@gmail.com>
2021-01-27 19:43:39 +0200

    Add Theme repository

---
+++ src/main/java/ua/od/ones/model/repository/ThemeRepository.java
@@ -0,0 +1,16 @@
+package ua.od.ones.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.od.ones.model.entity.Role;
+import ua.od.ones.model.entity.Theme;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ThemeRepository extends JpaRepository <Theme, Long> {
+    Optional<Theme> findById(Long id);
+
+    Optional<Theme> findByName(String name);
+
+    List<Theme> findAll();
+}
---
+++ src/main/java/ua/od/ones/model/service/impl/RoleServiceImpl.java
@@ -0,0 +1,61 @@
+package ua.od.ones.model.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.od.ones.model.entity.Role;
+import ua.od.ones.model.repository.RoleRepository;
+import ua.od.ones.model.service.RoleService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    RoleRepository repository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public boolean delete(long id) {
+        if(isExists(id)) {
+            repository.deleteById(id);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isExists(long id) {
+        return repository.existsById(id);
+    }
+
+    @Override
+    public Role create(Role role) {
+        if(role.getId() == 0) {
+            return repository.saveAndFlush(role);
+        }
+        return role;
+    }
+
+    @Override
+    public boolean update(Role role) {
+        if(role.getId() == 0) {
+            repository.saveAndFlush(role);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Role> getById(long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Role> getAll() {
+        return repository.findAll();
+    }
+}
